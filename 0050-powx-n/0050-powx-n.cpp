e6922779a1235762cb7f1363fbff3826c6353d48
Saniya <146199900+Saniya23-Tech@users.noreply.github.com>
2026-07-29 20:56:13 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.18%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long binForm = n;   
+        if (binForm < 0) {  
+            x = 1 / x;
+            binForm = -binForm; //converting negative to positive 
+        }
+
+        double ans = 1.0;
+        while (binForm > 0) {
+            if (binForm % 2 == 1) {  
+                ans *= x;
+            }
+            x *= x;           
+            binForm /= 2; 
+        }
+        return ans;
+    }
+};
